8e4a938d9916022bbdadade581f69477ae8d0a56
John doecd <julien.gioria@oriolis.fr>
2016-07-06 17:13:53 +0200

    service de redirection en fonction du role de l'identifiant.

---
+++ src/AppBundle/Service/SuccessHandler.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*
+* URL Source : http://benjamin.leveque.me/symfony2-redirection-de-l-utilisateur-en-fonction-de-son-role.html
+*
+*/
+
+namespace AppBundle\Service;
+
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class SuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    protected $router;
+
+    public function __construct(RouterInterface $router, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->router = $router;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        if ($this->authorizationChecker->isGranted('ROLE_ADMINISTRATOR')) {
+            return new RedirectResponse($this->router->generate('admin'));
+        }
+        else {
+            return new RedirectResponse($this->router->generate('dashboard'));
+        }
+    }
+}
+?>
